cda6406d355bd4f220af3c7c030ab2525b1ff355
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-09 15:35:42 -0500

    Add listen to index.ts to start server

--- index.ts
+++ index.ts
@@ -13,3 +13,8 @@ app.use(express.json({ limit: "50mb" }));
 app.get("/", async (req, res) => {
   res.send("Hello from DALL-E!");
 });
+const port = 3000;
+
+app.listen(port, () => {
+  return console.log(`Express is listening at http://localhost:${port}`);
+});
