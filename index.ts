b11a38306232702cb3c4ee443be23aa0edde6731
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-11 11:40:53 -0500

    Set up mongodb

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./mongodb/connect";
+
 dotenv.config();
 
 const app = express();
@@ -14,7 +16,17 @@ app.get("/", async (req, res) => {
   res.send("Hello from DALL-E!");
 });
 
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(process.env.PORT, () => {
+      return console.log(
+        `Server is listening at http://localhost:${process.env.PORT}`
+      );
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.listen(process.env.PORT, () => {
-  return console.log(`Express is listening at http://localhost:${process.env.PORT}`);
-});
+startServer();
