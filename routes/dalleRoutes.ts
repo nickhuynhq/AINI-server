846fc5dfe3cb9850e83a7fd1fa058d5ae58218ba
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-12 13:05:23 -0500

    Format and style CreatePosts page for mobile and desktop

--- routes/dalleRoutes.ts
+++ routes/dalleRoutes.ts
@@ -20,16 +20,27 @@ router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
 
-    const aiResponse = await openai.createImage({
+    const aiImageResponse = await openai.createImage({
       prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
 
-    const image = aiResponse.data.data[0].b64_json;
+    const aiStoryResponse = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: `Create a short story using this prompt: ${prompt}`,
+      temperature: 0.7,
+      max_tokens: 512,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    });
 
-    res.status(200).json({ photo: image });
+    const image = aiImageResponse.data.data[0].b64_json;
+    const text = aiStoryResponse.data.choices[0].text;
+
+    res.status(200).json({ photo: image, story: text });
   } catch (error) {
     console.log(error);
     res.status(500).send(error?.response.data.error.message);
