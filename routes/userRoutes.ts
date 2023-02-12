97b509bd07bce7ca6e5d3447dc9e37304e574d66
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-12 16:53:09 -0500

    Set up cloudinary to host images

---
+++ routes/userRoutes.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import { v2 as cloudindary } from "cloudinary";
+
+dotenv.config();
+
+const router = express.Router();
+
+export default router;
