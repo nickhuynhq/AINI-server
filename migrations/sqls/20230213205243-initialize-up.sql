7963bf21780ff7d35f552e477dfccb2dcb5f40e2
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-13 19:50:25 -0500

    Add register route and validation for db

--- migrations/sqls/20230213205243-initialize-up.sql
+++ migrations/sqls/20230213205243-initialize-up.sql
@@ -2,15 +2,21 @@
 -- Table: public.users
 
 -- DROP TABLE IF EXISTS public.users;
+DROP EXTENSION IF EXISTS citext cascade;
+CREATE EXTENSION citext;
+CREATE DOMAIN domain_email AS citext
+CHECK(
+   VALUE ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$'
+);
 
 CREATE TABLE IF NOT EXISTS public.users
 (
-    id bigint NOT NULL,
+    id SERIAL NOT NULL,
     firstname character varying(256) COLLATE pg_catalog."default" NOT NULL,
     lastname character varying(256) COLLATE pg_catalog."default" NOT NULL,
-    email character varying(256) COLLATE pg_catalog."default" NOT NULL,
-    password character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    email domain_email COLLATE pg_catalog."default" NOT NULL,
     username character varying(256) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(256) COLLATE pg_catalog."default" NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (id)
 )
 
--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -1,12 +1,66 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import db from "../queries/userQueries";
+import { pool } from "../queries/query";
+import bcrypt from "bcrypt";
 
 dotenv.config();
 
 const router = express.Router();
 
-// Postgres query
-router.route("/").get(db.getUsers);
+router.get("/", async (req, res) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+});
+
+router.get("/:userId", async (req, res) => {
+  pool.query(
+    `SELECT * FROM users WHERE id=${req.params.userId} ORDER BY id ASC`,
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows);
+    }
+  );
+});
+
+router.post("/register", async (req, res) => {
+    const selectQuery = `
+      SELECT username
+      FROM users
+      WHERE username = $1
+    `;
+  
+    const selectValues = [req.body.username];
+    const { rows: existingUsernames } = await pool.query(selectQuery, selectValues);
+  
+    if (existingUsernames.length > 0) {
+      return res.status(403).json({ message: "Username is already taken" });
+    }
+  
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(req.body.password, salt);
+  
+    const insertQuery = `
+      INSERT INTO users (firstname, lastname, email, password, username)
+      VALUES ($1, $2, $3, $4, $5)
+    `;
+  
+    const insertValues = [
+      req.body.firstname,
+      req.body.lastname,
+      req.body.email,
+      hash,
+      req.body.username
+    ];
+  
+    await pool.query(insertQuery, insertValues);
+    res.status(200).json({ message: "Successfully registered" });
+  });
+  
 
 export default router;
