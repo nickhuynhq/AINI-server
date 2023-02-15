120c18d286304db0704244e8735ab601b637a287
Nicholas Huynh <nickhuynhq@gmail.com>
2023-02-15 14:09:31 -0500

    Add posts table

--- migrations/sqls/20230213205243-initialize-down.sql
+++ migrations/sqls/20230213205243-initialize-down.sql
@@ -1,3 +1,4 @@
 /* Replace with your SQL commands */
 
-DROP TABLE IF EXISTS public.users
+DROP TABLE IF EXISTS public.users cascade;
+DROP TABLE IF EXISTS public.posts cascade;
--- migrations/sqls/20230213205243-initialize-up.sql
+++ migrations/sqls/20230213205243-initialize-up.sql
@@ -9,6 +9,7 @@ CHECK(
    VALUE ~ '^[a-zA-Z0-9.!#$%&''*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$'
 );
 
+-- Users: Main table of the database that contains the information of the users registered in our application. The data that will be stored in this table will be the name
 CREATE TABLE IF NOT EXISTS public.users
 (
     id serial PRIMARY KEY NOT NULL,
@@ -21,7 +22,19 @@ CREATE TABLE IF NOT EXISTS public.users
     created_at timestamp with time zone NOT NULL DEFAULT now()
 );
 
-
+-- Posts: Database table with all the posts from all the users. Each post will contain the title, description, and the main content of the post.
+CREATE TABLE IF NOT EXISTS public.posts
+(
+    id serial PRIMARY KEY NOT NULL,
+    title TEXT NOT NULL,
+    picture TEXT NOT NULL,
+    description TEXT NOT NULL,
+    created_at timestamp with time zone NOT NULL DEFAULT (NOW()),
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);
 
 ALTER TABLE IF EXISTS public.users
+    OWNER to postgres;
+
+ALTER TABLE IF EXISTS public.posts
     OWNER to postgres;
